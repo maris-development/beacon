02153bdc7b5ca7ab49c0530b0a763087e86b6007
Robin Kooyman <robin.kooyman.work@gmail.com>
2025-03-25 21:56:00 +0100

    rename map

--- beacon-functions/src/blue_cloud/map_cora_instrument_l05.rs
+++ beacon-functions/src/blue_cloud/map_cora_instrument_l05.rs
@@ -12,7 +12,7 @@ use datafusion::{
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref L06_MAP: HashMap<i64, &'static str> = {
+    static ref L05_MAP: HashMap<i64, &'static str> = {
         let mut map = HashMap::new();
         map.insert(810, "SDN:L05::30");
         map.insert(820, "SDN:L05::135");
@@ -66,7 +66,7 @@ fn map_cora_instrument_l05_impl(
                 .unwrap();
 
             let array = flag_array.iter().map(|flag| {
-                flag.map(|wmo_code| L06_MAP.get(&wmo_code).map(|s| s).cloned())
+                flag.map(|wmo_code| L05_MAP.get(&wmo_code).map(|s| s).cloned())
                     .flatten()
             });
 
@@ -76,7 +76,7 @@ fn map_cora_instrument_l05_impl(
         }
         ColumnarValue::Scalar(ScalarValue::Int64(value)) => {
             let sdn_flag = value
-                .map(|wmo_code| L06_MAP.get(&wmo_code).map(|s| s.to_string()))
+                .map(|wmo_code| L05_MAP.get(&wmo_code).map(|s| s.to_string()))
                 .flatten();
 
             Ok(ColumnarValue::Scalar(
