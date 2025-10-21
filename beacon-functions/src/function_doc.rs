2eb1c9f81af6d7af38524c07d1473feeb6d6f3ad
Robin Kooyman <robin.kooyman.work@gmail.com>
2025-10-21 11:56:28 +0200

    wip

--- beacon-functions/src/function_doc.rs
+++ beacon-functions/src/function_doc.rs
@@ -1,4 +1,4 @@
-use datafusion::logical_expr::ScalarUDF;
+use datafusion::{functions::core::coalesce::CoalesceFunc, logical_expr::ScalarUDF};
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct FunctionDoc {
@@ -18,58 +18,80 @@ pub struct FunctionParameter {
 impl FunctionDoc {
     pub fn from_scalar(scalar: &ScalarUDF) -> Vec<Self> {
         let documentation = scalar.documentation();
-        scalar
-            .signature()
-            .type_signature
-            .get_example_types()
-            .iter()
-            .map(|data_types| {
-                let return_type = scalar.return_type(data_types);
-                let return_type_str = return_type
-                    .map(|dt| format!("{dt}"))
-                    .unwrap_or_else(|_| "unknown".to_string());
-                let mut function_doc = FunctionDoc {
-                    function_name: scalar.name().to_string(),
-                    description: "No documentation available".to_string(),
-                    return_type: return_type_str,
-                    params: vec![],
-                };
-                if let Some(doc) = documentation {
-                    function_doc.description = doc.description.clone();
-                    if let Some(arguments) = doc.arguments.as_ref() {
-                        let params = arguments
-                            .iter()
-                            .zip(data_types.iter())
-                            .map(|((name, description), data_type)| FunctionParameter {
-                                name: name.clone(),
-                                description: description.clone(),
-                                data_type: format!("{data_type}"),
-                            })
-                            .collect();
-                        FunctionDoc {
-                            params,
-                            ..function_doc
+        let signature = scalar.signature();
+        if !signature.type_signature.get_example_types().is_empty() {
+            signature
+                .type_signature
+                .get_example_types()
+                .iter()
+                .map(|data_types| {
+                    let return_type = scalar.return_type(data_types);
+                    let return_type_str = return_type
+                        .map(|dt| format!("{dt}"))
+                        .unwrap_or_else(|_| "unknown".to_string());
+                    let mut function_doc = FunctionDoc {
+                        function_name: scalar.name().to_string(),
+                        description: "No documentation available".to_string(),
+                        return_type: return_type_str,
+                        params: vec![],
+                    };
+                    if let Some(doc) = documentation {
+                        function_doc.description = doc.description.clone();
+                        if let Some(arguments) = doc.arguments.as_ref() {
+                            let params = arguments
+                                .iter()
+                                .zip(data_types.iter())
+                                .map(|((name, description), data_type)| FunctionParameter {
+                                    name: name.clone(),
+                                    description: description.clone(),
+                                    data_type: format!("{data_type}"),
+                                })
+                                .collect();
+                            FunctionDoc {
+                                params,
+                                ..function_doc
+                            }
+                        } else {
+                            // No argument documentation available so we just fill in the types
+                            let params = data_types
+                                .iter()
+                                .enumerate()
+                                .map(|(i, data_type)| FunctionParameter {
+                                    name: format!("arg{}", i + 1),
+                                    description: "No description available".to_string(),
+                                    data_type: format!("{data_type}"),
+                                })
+                                .collect();
+                            FunctionDoc {
+                                params,
+                                ..function_doc
+                            }
                         }
                     } else {
-                        // No argument documentation available so we just fill in the types
-                        let params = data_types
-                            .iter()
-                            .enumerate()
-                            .map(|(i, data_type)| FunctionParameter {
-                                name: format!("arg{}", i + 1),
-                                description: "No description available".to_string(),
-                                data_type: format!("{data_type}"),
-                            })
-                            .collect();
-                        FunctionDoc {
-                            params,
-                            ..function_doc
-                        }
+                        function_doc
                     }
-                } else {
-                    function_doc
+                })
+                .collect()
+        } else {
+            let mut params = vec![];
+            if let Some(arguments) = documentation.and_then(|doc| doc.arguments.as_ref()) {
+                for (i, (name, description)) in arguments.iter().enumerate() {
+                    params.push(FunctionParameter {
+                        name: name.clone(),
+                        description: description.clone(),
+                        data_type: "unknown".to_string(),
+                    });
                 }
-            })
-            .collect()
+            }
+
+            vec![FunctionDoc {
+                function_name: scalar.name().to_string(),
+                description: documentation
+                    .map(|doc| doc.description.clone())
+                    .unwrap_or("No documentation available".to_string()),
+                return_type: "unknown".to_string(),
+                params,
+            }]
+        }
     }
 }
