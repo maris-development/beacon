be6cf26480899f266d9ca7ef024994d9eb9c120e
Robin Kooyman <robin.kooyman.work@gmail.com>
2026-01-22 11:17:39 +0100

    Add argo platform edmo mappings (#163)

--- beacon-functions/src/blue_cloud/argo/mod.rs
+++ beacon-functions/src/blue_cloud/argo/mod.rs
@@ -1,8 +1,10 @@
 pub mod map_instrument_l05;
+pub mod map_platform_edmo;
 pub mod map_platform_l06;
 
 pub fn argo_udfs() -> Vec<datafusion::logical_expr::ScalarUDF> {
     vec![
+        map_platform_edmo::map_argo_platform_edmo(),
         map_instrument_l05::map_argo_instrument_l05(),
         map_platform_l06::map_argo_platform_l06(),
     ]
