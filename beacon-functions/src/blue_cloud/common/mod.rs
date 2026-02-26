652de035ff5dad4d69c2a82e861312906c65d284
Robin Kooyman <robin.kooyman.work@gmail.com>
2026-02-26 11:08:40 +0100

    Add additional blue cloud mappings (#171)
    * Add world ocean database mappings

--- beacon-functions/src/blue_cloud/common/mod.rs
+++ beacon-functions/src/blue_cloud/common/mod.rs
@@ -3,6 +3,8 @@ pub mod map_c17_l06;
 pub mod map_call_sign_c17;
 pub mod map_l22_l05;
 pub mod map_measuring_area_type_feature_type;
+pub mod map_p01_p25;
+pub mod map_p25_l05;
 pub mod map_wmo_instrument_type_l05;
 pub mod map_wmo_instrument_type_l22;
 pub mod pressure_to_depth_teos_10;
