1bc0a0db53d92b168c9d698592ce4dd2c95bf5d8
Robin Kooyman <robin.kooyman.work@gmail.com>
2026-05-11 14:09:36 +0200

    index page

---
+++ docs/.vitepress/theme/components/IntegrationBadges.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="integration-strip">
+    <span class="integration-strip-label">Works with</span>
+
+    <span
+      v-for="integration in integrations"
+      :key="integration"
+      class="integration-strip-item"
+    >
+      {{ integration }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+const integrations = [
+  'DataGrip',
+  'DBeaver',
+  'JDBC',
+  'Arrow Flight SQL',
+  'Python',
+  'Dask',
+  'Jupyter Notebooks'
+]
+</script>
