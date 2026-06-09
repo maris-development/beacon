9b0fd84cb492ef3ba88fdeedc38b65f0b742342e
Robin Kooyman <robin.kooyman.work@gmail.com>
2026-06-09 15:39:09 +0200

    wip

--- beacon-functions/src/blue_cloud/seadatanet/mod.rs
+++ beacon-functions/src/blue_cloud/seadatanet/mod.rs
@@ -1,5 +1,7 @@
 pub mod map_c17_l06;
 pub mod map_instrument_l05;
+pub mod map_instrument_l05_salinity;
+pub mod map_instrument_l05_temperature;
 pub mod map_originator_edmo;
 pub mod map_platform_l06;
 pub mod map_units;
@@ -9,6 +11,8 @@ pub fn seadatanet_udfs() -> Vec<datafusion::logical_expr::ScalarUDF> {
         map_c17_l06::map_platform_c17_l06(),
         map_platform_l06::map_seadatanet_platform_l06(),
         map_instrument_l05::map_seadatanet_instrument_l05(),
+        map_instrument_l05_salinity::map_seadatanet_instrument_l05_salinity(),
+        map_instrument_l05_temperature::map_seadatanet_instrument_l05_temperature(),
         map_originator_edmo::map_originator_edmo(),
         map_units::map_units(),
     ]
